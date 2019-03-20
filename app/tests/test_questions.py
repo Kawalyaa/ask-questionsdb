55ab051041dbd395d585396eb6ceaecde8e29284
Kawalya Andrew <andrew58934@gmail.com>
2019-03-20 13:37:42 +0300

    perfect user test

--- app/tests/test_questions.py
+++ app/tests/test_questions.py
@@ -1,112 +1,68 @@
 import json
 import unittest
-
+# from app.db_con import DataBaseConnection
+from app.tests.test_user2 import BaseTest
 from app import creat_app
-# from app.db_con import destroydb, init_tdb
+# db_uri = DataBaseConnection("dbname='question_test' host='localhost' port=5432  user='kawalya' password='kawalyaa'")
 
 
 class TestUser(unittest.TestCase):
     """docstring for TestUser"""
-    def create_user(self):
-        """create a fictitious user"""
-        # username = "".join(choice(
-        #                   string.ascii_letters) for x in range(randint(7, 10)))
-        data = {
-            "name": "Andrew",
-            "user_name": "devperandrew5",
-            "password": "bornagain",
-            "email": "andrew5@gmail.com"
-        }
-        path = "/api/v2/auth/signup"
-        user = self.client.post(path,
-                                data=json.dumps(data),
-                                content_type="application/json")
-
-        # user_id = user.json['user_id']
-        auth_token = user.json['access_token']
-        return auth_token
 
     def setUp(self):
+        # db_uri.drop_all_tables()
+        # db_uri.creat_tables()
         self.app = creat_app('testing')
         self.client = self.app.test_client()
-
-        self.question = {
-            "title": "Test  3 ",
-            "description": "Lorem ",
+        with self.app.app_context():
+            self.db = db_uri.creat_tables()
+        self.data = {
+            "title": "WORLDI NEWZ",
+            "description": "Is CCTV found in central china!!",
             "created_by": 4
         }
 
-    def post_data(self, path='/api/v2/question', auth_token=2, data={}, headers=0):
-        """This function performs a POST request using the testing client"""
+    def register_user(self, path="/api/v2/auth/signup", data={}):
+        db_uri.drop_all_tables()
         if not data:
-            data = self.question
-        if auth_token is 2:
-            user = self.create_user()
-            auth_token = user[1]
-        if not headers:
-            headers = {"Authorization": "Bearer {}".format(auth_token)}
-        result = self.client.post(path, data=json.dumps(data),
-                                  headers=headers,
-                                  content_type='application/json')
-        return result
+            data = {
+                "name": "Kawalya",
+                "user_name": "andrewhi",
+                "email": "andrew5@aaaa.com",
+                "password": "bornagain"
+            }
+            login = {
+                "user_name": "andrewhi",
+                "password": "bornagain"
+            }
+        header = {"content-type": "application/json"}
+        res = self.client.post(path=path, data=json.dumps(data), headers=header)
+        res = self.client.post(path="/api/v2/auth/login", data=json.dumps(login), headers={"content-type": "application/json"})
+        return res.json['access_token']
 
-    # def register_user(self, path="/api/v2/auth/signup", data={}):
-    #    if not data:
-    #        data = {
-    #            "name": "Andrew",
-    #            "user_name": "devperandrew5",
-    #            "password": "bornagain",
-    #            "email": "andrew5@gmail.com"
-    #        }
-    #    header = {"content-type": "application/json"}
-    #    res = self.client.post(path=path, data=json.dumps(data), headers=header)
-    #    return res["access_token"]
+    def post(self, path="/api/v2/question", data={}):
+        if not data:
+            data = self.data
+        token = self.register_user()
+        header = {
+            "Authorization": "Bearer {}".format(token),
+            "content-type": "application/json"
+        }
+        res = self.client.post(path=path, data=json.dumps(data), headers=header)
+        return res
 
-    # def post(self, path="/api/v2//question", data={}):
-    #    if not data:
-    #    token = self.register_user()
-    #    header = {
-    #        "Authorization": "Bearer {}".format(token),
-    #        "content-type": "application/json"
-    #    }
-    #    res = self.client.post(path=path, data=json.dumps(data), headers=header)
-    #    return res
-    def get_data(self, path='/api/v2/question'):
-        """This function performs a GET request to a given path
-            using the testing client
-        """
-        result = self.client.get(path)
-        return result
+    def get(self, path="/api/v2/question", data={}):
+        token = self.register_user()
+        header = {
+            "Authorization": "Bearer {}".format(token),
+            "content-type": "application/json"
+        }
+        res = self.client.get(path=path, headers=header)
+        return res
 
-    # def get(self, path="/api/v2//question", data={}):
-    #    token = self.register_user()
-    #    header = {
-    #        "Authorization": "Bearer {}".format(token),
-    #        "content-type": "application/json"
-    #    }
-    #    res = self.client.get(path=path, headers=header)
-    #    return res
-
-    def test_post_question(self):
-        """Test that a user can post a question
-        """
-        new_question = self.post_data()
-        # test that the server responds with the correct status code
-        self.assertEqual(new_question.status_code, 201)
-        # self.assertTrue(new_question.json['message'])
-        # self.assertTrue(new_question.json['question_id'])
-
-    # def test_posting_blog(self):
-    #    res = self.post()
-    #    self.assertEqual(res.status_code, 201)
-
-    def test_get_questions(self):
-        """Test that the api can respond with a list of questions"""
-        new_question = self.post_data()
-        questions = self.get_data().json
-        self.assertEqual(questions.status_code, 200)
-        # self.assertEqual(questions['message'], 'success')
-        # self.assertIn(new_question.json['text'], str(questions['questions']))
+    def test_posting_blog(self):
+        res = self.post()
+        self.assertEqual(res.status_code, 201)
 
     def test_getting_blog(self):
         res = self.get()
--- app/tests/test_user2.py
+++ app/tests/test_user2.py
@@ -3,8 +3,10 @@ import json
 import unittest
 # import psycopg2
 # from instance.config import app_config
+from app.db_con import DataBaseConnection
 from app import creat_app
 secret = os.getenv("SECRET")
+db_uri = DataBaseConnection("dbname='question_test' host='localhost' port=5432  user='kawalya' password='kawalyaa'")
 
 
 class BaseTest(unittest.TestCase):
@@ -12,17 +14,24 @@ class BaseTest(unittest.TestCase):
 
     def setUp(self):
         """ set the variables before each test """
+        db_uri.drop_all_tables()
+        db_uri.creat_tables()
         self.app = creat_app('testing')
         self.client = self.app.test_client()
         self.data = {
-            "name": "AbrahamOgol",
-            "user_name": "aogollou",
-            "email": "a5@aaaa.com",
-            "password": "mcogols"
+            "name": "Kawalya",
+            "user_name": "andrewhi",
+            "email": "andrew5@aaaa.com",
+            "password": "bornagain"
+        }
+
+        self.login = {
+            "user_name": "andrewhi",
+            "password": "bornagain"
         }
 
     def tearDown(self):
-        pass
+        db_uri.drop_all_tables()
 
 
 class UserSignUp(BaseTest):
@@ -30,12 +39,11 @@ class UserSignUp(BaseTest):
     def test_user_signup(self):
         resp = self.client.post(path='/api/v2/auth/signup', data=json.dumps(self.data), headers={"content-type": "application/json"})
         self.assertEqual(resp.status_code, 201)
-        pass
 
     def test_user_login(self):
-        res = self.client.post(path="/api/v2/auth/login", data=json.dumps(self.data), headers={"content-type": "application/json"})
-        self.assertEqual(res.status_code, 401)
-        pass
+        res = self.client.post(path='/api/v2/auth/signup', data=json.dumps(self.data), headers={"content-type": "application/json"})
+        res = self.client.post(path="/api/v2/auth/login", data=json.dumps(self.login), headers={"content-type": "application/json"})
+        self.assertEqual(res.status_code, 200)
 
     def test_login_unexisting_user(self):
         data = {
